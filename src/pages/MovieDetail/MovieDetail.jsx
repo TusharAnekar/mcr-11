422a3ce2b4c82812e91e5225a202c4541b4af710
Tushar Anekar <anekar.tushar@gmail.com>
2023-08-13 15:05:57 +0530

    fix- star and watchlist

--- src/pages/MovieDetail/MovieDetail.jsx
+++ src/pages/MovieDetail/MovieDetail.jsx
@@ -6,10 +6,14 @@ import { MoviesContext } from "../../contexts/movies-context";
 export function MovieDetail() {
   const { movieId } = useParams();
   const {
-    moviesState: { movies },
+    moviesState: { movies, watchList, starredMovies },
+    moviesDisptach,
+    getIsMovieInWatchlist,
+    getIsMovieInStarredMovies,
   } = useContext(MoviesContext);
   const movie = movies.find(({ id }) => id === Number(movieId));
   const {
+    id,
     title,
     year,
     genre,
@@ -21,6 +25,31 @@ export function MovieDetail() {
     imageURL,
   } = movie;
 
+  const isMovieInWatchlist = getIsMovieInWatchlist(id);
+  const isMovieInStarredMovies = getIsMovieInStarredMovies(id);
+
+  function handleAddToWishlist() {
+    if (isMovieInWatchlist) {
+      moviesDisptach({
+        type: "REMOVE_FROM_WATCHLIST",
+        payload: watchList.filter((movie) => movie.id !== id),
+      });
+    } else {
+      moviesDisptach({ type: "ADD_TO_WISHLIST", payload: movie });
+    }
+  }
+
+  function handleAddToStarredMovies() {
+    if (isMovieInStarredMovies) {
+      moviesDisptach({
+        type: "REMOVE_FROM_STARRED_MOVIES",
+        payload: starredMovies.filter((movie) => movie.id !== id),
+      });
+    } else {
+      moviesDisptach({ type: "ADD_TO_STARRED_MOVIES", payload: movie });
+    }
+  }
+
   return (
     <div className="movie-detail-container">
       <div className="movie-detail-card-container">
@@ -29,7 +58,9 @@ export function MovieDetail() {
           <h3>{title}</h3>
           <p>{summary}</p>
           <p>Year: {year}</p>
-          <div className="genre-content"> Genre: 
+          <div className="genre-content">
+            {" "}
+            Genre:
             {genre.map((item, index) => (
               <p key={index}>{item}</p>
             ))}
@@ -37,14 +68,20 @@ export function MovieDetail() {
           <p>Rating: {rating}</p>
           <p>Director: {director}</p>
           <p>Writer: {writer}</p>
-          <div className="cast-content"> Cast:
+          <div className="cast-content">
+            {" "}
+            Cast:
             {cast.map((item, index) => (
               <p key={index}>{item}</p>
             ))}
           </div>
           <div className="buttons-container">
-            <button>Star</button>
-            <button>Add To Wishlist</button>
+            <button onClick={handleAddToStarredMovies}>
+              {isMovieInStarredMovies ? "UnStar" : "Star"}
+            </button>
+            <button onClick={handleAddToWishlist}>
+              {isMovieInWatchlist ? "Remove From Watchlist" : "Add to Wishlist"}
+            </button>
           </div>
         </div>
       </div>
